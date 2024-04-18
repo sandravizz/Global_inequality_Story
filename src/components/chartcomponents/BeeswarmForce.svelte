d499b7f3c33ea2e293dd51d34a84d8dba872005d
Patrick Wojda <pewe@PPP.lan>
2024-04-18 11:52:03 +0200

    updated the beeswarm so highlights are possible

--- src/components/chartcomponents/BeeswarmForce.svelte
+++ src/components/chartcomponents/BeeswarmForce.svelte
@@ -3,21 +3,15 @@
 	Generates an SVG Beeswarm chart using a [d3-force simulation](https://github.com/d3/d3-force).
  -->
 <script>
-	import { getContext } from 'svelte';
-	import { forceSimulation, forceX, forceY, forceCollide } from 'd3-force';
+	import { getContext } from "svelte";
+	import { forceSimulation, forceX, forceY, forceCollide } from "d3-force";
 
-	const { data, xGet, height, zGet } = getContext('LayerCake');
+	const { data, xGet, height, zGet } = getContext("LayerCake");
 
 	const nodes = $data.map((d) => ({ ...d }));
 
 	/** @type {Number} [r=4] - The circle radius size in pixels. */
 	export let r = 4;
-
-	/** @type {Number} [strokeWidth=1] - The circle's stroke width in pixels. */
-	export let strokeWidth = 1;
-
-	/** @type {String} [stroke='#fff'] - The circle's stroke color. */
-	export let stroke = '#fff';
 
 	/** @type {Number} [xStrength=0.95] - The value passed into the `.strength` method on `forceX`. See [the documentation](https://github.com/d3/d3-force#x_strength). */
 	export let xStrength = 0.95;
@@ -28,31 +22,62 @@
 	/** @type {Function} [getTitle] — An accessor function to get the field on the data element to display as a hover label using a `<title>` tag. */
 	export let getTitle = undefined;
 
+	export let highlightValue = undefined;
+	export let highlightKey = undefined;
+
 	$: simulation = forceSimulation(nodes)
-		.force('x', forceX().x(d => $xGet(d)).strength(xStrength))
-		.force('y', forceY().y($height / 2).strength(yStrength))
-		.force('collide', forceCollide(r))
+		.force(
+			"x",
+			forceX()
+				.x((d) => $xGet(d))
+				.strength(xStrength)
+		)
+		.force(
+			"y",
+			forceY()
+				.y($height / 2)
+				.strength(yStrength)
+		)
+		.force("collide", forceCollide(r + 1))
 		.stop();
 
 	$: {
-		for ( let i = 0,
-			n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay()));
+		for (
+			let i = 0,
+				n = Math.ceil(
+					Math.log(simulation.alphaMin()) /
+						Math.log(1 - simulation.alphaDecay())
+				);
 			i < n;
-			++i ) {
+			++i
+		) {
 			simulation.tick();
 		}
 	}
 </script>
 
-<g class='bee-group'>
+<g class="bee-group">
 	{#each simulation.nodes() as node}
 		<circle
-			fill='{$zGet(node)}'
-			stroke='{stroke}'
-			stroke-width='{strokeWidth}'
-			cx='{node.x}'
-			cy='{node.y}'
-			r='{r}'
+			fill={highlightValue
+				? node[highlightKey] === highlightValue
+					? $zGet(node)
+					: "#222"
+				: $zGet(node)}
+			fill-opacity={highlightValue
+				? node[highlightKey] === highlightValue
+					? 0.8
+					: 0.15
+				: 0.8}
+			stroke={highlightValue
+				? node[highlightKey] === highlightValue
+					? $zGet(node)
+					: "none"
+				: $zGet(node)}
+			stroke-width="1"
+			cx={node.x}
+			cy={node.y}
+			{r}
 		>
 			{#if getTitle}
 				<title>{getTitle(node)}</title>
@@ -60,3 +85,9 @@
 		</circle>
 	{/each}
 </g>
+
+<style>
+	circle {
+		transition: all 0.7s;
+	}
+</style>
