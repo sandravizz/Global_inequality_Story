3e4ee9d33a5121b40f04a6ebdcb1163e949b521f
Sandra Becker <sandra.becker1982@gmail.com>
2025-12-02 13:45:05 +0000

    Updates

---
+++ app/immutable/chunks/paths.BssS4yKY.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_18c5jll)==null?void 0:s.base)??"/GLOBAL-INEQUALITY-STORY";var l;const e=((l=globalThis.__sveltekit_18c5jll)==null?void 0:l.assets)??a;export{e as a,a as b};
--- app/immutable/entry/start.DeJaIWDe.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DDoOPxer.js";export{t as start};
