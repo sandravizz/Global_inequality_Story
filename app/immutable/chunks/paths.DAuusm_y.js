0a4a12c3a91a010b90777cc6eb3e306b711ada45
Patrick Wojda <patrick@datastory.org>
2025-12-04 22:24:25 +0100

    Updates

---
+++ app/immutable/chunks/paths.DAuusm_y.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_y0g9w0)==null?void 0:s.base)??"/GLOBAL-INEQUALITY-STORY";var a;const t=((a=globalThis.__sveltekit_y0g9w0)==null?void 0:a.assets)??e;export{t as a,e as b};
